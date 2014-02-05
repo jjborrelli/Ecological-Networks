c4d1f6d730b777ac5e01d31bb091b0c7a257951c
JJ Borrelli <borrejj@gmail.com>
2014-02-05 15:57:59 -0500

    new permutation tests, update directories

--- Food Webs/Rscripts/web_analysis.R
+++ Food Webs/Rscripts/web_analysis.R
@@ -3,20 +3,18 @@ library(NetIndices)
 library(reshape2)
 library(ggplot2)
 library(devtools)
+library(vegan)
 
-
-### Restore workspace    -------------------------------------------------
-                                                                         
-setwd("~/Desktop/GitHub/Ecological Networks/Projects/Food Webs/Rdata")   
-load("foodweb.Rdata")                                                    
+### Load functions       --------------------------------------------------
+setwd("~/Desktop/GitHub/Ecological-Networks/Food Webs")
 
 ### Load functions       --------------------------------------------------
 
-url <- "https://raw.github.com/jjborrelli/Ecological-Networks/master/Projects/Food%20Webs/Rscripts/web_functions.R"
+url <- "https://raw.github.com/jjborrelli/Ecological-Networks/master/Food%20Webs/Rscripts/web_functions.R"
 source_url(url)
 
 ### Import food web data  --------------------------------------------------
-inputs <- get_webs("~/Desktop/GitHub/Ecological Networks/Database/Food_Web/Edgelist")
+inputs <- get_webs("~/Dropbox/Food Web Database/Food_Web/Edgelist")
 web.graphs <- inputs$graph.list
 web.matrices <- inputs$adjacency.list
 webnames <- inputs$webnames
@@ -24,13 +22,15 @@ webnames <- inputs$webnames
 ### Calculate common food web indices/statistics  ----------------------------
 fw.indices <- get_fw_indices(web.matrices, web.graphs, webnames)
 fw.indices[1:2,]
+#write.csv(fw.indices, file = "Tables/FWindices.csv")
+fw.indices <- read.csv("Tables/FWindices.csv")
 
 # Calculate node properties of each food web  ------------------------------
 node.props <- get_node_properties(web.matrices, webnames)
 
 # Save node.props as a csv files
-setwd("~/Desktop/GitHub/Ecological Networks/Projects/Chain Length/Rdata")
-write.csv(node.props, "node properties.csv")
+#write.csv(node.props, file = "Tables/NODEproperties.csv")
+node.props <- read.csv("Tables/NODEproperties.csv")
 
 # Create subset of nodes that are herbivores or higher in trophic position
 consumers <- which(round(node.props$TL, 6) >= 2)
@@ -92,10 +92,15 @@ plot(predict(pca.fw)[,1:2])
 
 motif.df <- motif_counter(edge.graphs, webnames)
 
-motif.means <- apply(motif.df, 2, mean)
+#write.csv(motif.df, file = "Tables/motifCOUNTS.csv")
+motif.df <- read.table("Tables/motifCOUNTS.csv", header = T, sep = ",")
+
+motif.means <- apply(motif.df[,3:15], 2, mean)
 motif.sd <- apply(motif.df, 2, sd)
 
 motif.df.mc <- motif.df - motif.means         # Mean centered dataframe
+
+motif.quantiles <- apply(z.mat.norm, 2, quantile, probs = c(0.025, 0.975), na.rm = TRUE)
 
 # This function generates a list of lists of each of the graphs with rewired edges (preserving degree distribution)
 # It then loops through the list with the motif counter function created above
@@ -104,9 +109,10 @@ motif.df.mc <- motif.df - motif.means         # Mean centered dataframe
 # Here I am running the function on my 50 web list (web.graphs) with names = webnames
 # sample is the number of times I want to run the rewire function, iter is the number of rewiring iterations
 system.time(
-nulltest <- null_motifs(web.graphs, graph.names = webnames, sample = 1500, iter = 1000)
+nulltest <- null_motifs(web.graphs, graph.names = webnames, sample = 1000, iter = 1000)
 )
-## This took 105 minutes
+## This took 105 minutes, rep = 1500 
+## This took 22 minutes, rep = 1000
 
 nullmeans <- apply(nulltest[,2:14], 2, mean)
 nullsd <- apply(nulltest[,2:14], 2, sd)
@@ -129,7 +135,7 @@ nwm <- matrix(unlist(null.web.means), nrow = 50, ncol = 13, byrow = T)
 # Same for standard deviation
 nwsd <- matrix(unlist(null.web.sd), nrow = 50, ncol = 13, byrow = T)
 # Redefine the motif frequencies as a matrix
-mot.mat <- as.matrix(motif.df[,2:14])
+mot.mat <- as.matrix(motif.df[,3:15])
 
 # Calculate the z-score as (freq - mean_rewired) / sd_rewired
 z.mat <- (mot.mat - nwm) / nwsd
@@ -146,8 +152,10 @@ ggsave(filename = "motif_profile1.jpeg", height = 15, width = 15, units = "cm")
 
 zmatnorm.df <- melt(as.data.frame(z.mat.norm))
 mot.pro.norm <- ggplot(zmatnorm.df, aes(x = variable, y = value), na.rm = T) + geom_boxplot()
-mot.pro.norm + geom_hline(aes(yintercept = 0))
-ggsave(filename = "motif_profile2.jpeg", height = 15, width = 15, units = "cm")
+mot.pro.norm <- mot.pro.norm + geom_hline(aes(yintercept = 0)) 
+mot.pro.norm <- mot.pro.norm + geom_point(aes(x = 1:13, y = motif.quantiles[1,]), color = "blue")
+mot.pro.norm + geom_point(aes(x = 1:13, y = motif.quantiles[2,]), color = "blue")
+#ggsave(filename = "motif_profile2.jpeg", height = 15, width = 15, units = "cm")
 
 ###########
 ## Allesina and Pascual 2008 data
@@ -163,6 +171,43 @@ summary(fittest)
 nulldistr.web <- do.call(rbind, websplit)
 
 
-###  SAVE WORKSPACE 
-setwd("~/Desktop/GitHub/Ecological Networks/Projects/Rdata")
-save.image("foodweb.Rdata")
+###  Trying different null models ---------------------------------
+
+# Playing with the permatfull function
+?permatfull
+testing <- web.matrices[[1]]
+
+permuted2 <- permatfull(testing, fixedmar = "columns", mtype = "prab", times = 25)
+plot(permuted)
+
+permuted.graphs2 <- lapply(permuted2$perm, graph.adjacency)
+
+p.motif2 <- motif_counter(permuted.graphs2, webs = as.character(1:25))
+pmeans <- colMeans(p.motif2[2:14])
+
+## Write a function to use on a list of matrices
+web_permutation <- function(webmatrices, fixedmar, times){
+  require(vegan)
+  if (!is.list(webmatrices)){
+    webmatrices <- list(webmatrices)
+    warning("not a list")
+  }
+  len <- length(webmatrices)
+  p.motif <- list()
+  for (i in 1:len){
+    permuted <- permatfull(webmatrices[[i]], fixedmar = fixedmar, mtype = "prab", times = times)
+    permuted.graphs <- lapply(permuted$perm, graph.adjacency)
+    p.motif[[i]] <- motif_counter(permuted.graphs, webs = as.character(1:times))
+  }
+  return(p.motif)
+}
+
+permutes <- web_permutation(web.matrices, fixedmar = "both", times = 1000)
+p.means <- lapply(permutes, FUN = function(x){colMeans(x[,2:14])})
+pmeanmat <- matrix(unlist(p.means), ncol = 13, nrow = 50, byrow = T)
+p.sd <- lapply(permutes, FUN = function(x){apply(x[,2:14], 2, sd)})
+psdmat <- matrix(unlist(p.sd), ncol = 13, nrow = 50, byrow = T)
+
+zscor <- (mot.mat - pmeanmat) / psdmat
+znorm <- zscor/colSums(zscor^2, na.rm = T)
+boxplot(znorm)
