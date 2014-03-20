102b395e885e1f5caa829a4b415a142176e776b4
JJ Borrelli <borrejj@gmail.com>
2014-03-20 13:53:16 -0400

    add netindices and trophindices

--- Food Webs/Rscripts/bascompte_quantitative.R
+++ Food Webs/Rscripts/bascompte_quantitative.R
@@ -1,8 +1,9 @@
 # Bascompte et al. Quantitative network
 
 require(igraph)
+require(NetIndices)
 
-setwd("~/Desktop/GitHub/Ecological Networks/Database/Food_Web/Quantitative Network/")
+setwd("~/Dropbox/Food Web Database/Food_Web/Quantitative Network/")
 bmatrix <- read.csv("interactionsmarine.csv", header = TRUE, row.names = 1)
 bmatrix <- as.matrix(bmatrix)
 bgraph <- graph.adjacency(bmatrix, mode = "directed", weighted = TRUE)
@@ -28,7 +29,7 @@ ball <- graph.edgelist(bel[q0,])
 
 require(reshape2)
 require(devtools)
-source_url("https://raw.github.com/jjborrelli/Ecological-Networks/master/Projects/Food%20Webs/Rscripts/web_functions.R")
+source_url("https://raw.github.com/jjborrelli/Ecological-Networks/master/Food%20Webs/Rscripts/web_functions.R")
 
 bsubs <- list(b75, b50, b25, ball)
 motif.counts <- motif_counter(bsubs, webs = c("b75", "b50", "b25", "ball"))
@@ -52,3 +53,25 @@ require(ggplot2)
 troplot <- ggplot(bnodeprops, aes(x = TL)) 
 troplot <- troplot + geom_histogram(aes(y = ..density..), binwidth = .2, colour = "black", fill = "white") 
 troplot + facet_grid(L1 ~ .) + geom_density(alpha = .2, fill = "#FF6666")
+
+
+## Interaction strength based network subset indices ---------------------------------
+
+bas.graph.list <- list(b75, b50, b25, ball)
+bas.mat.list <- lapply(bas.graph.list, get.adjacency, sparse = F)
+sub.indices <- sapply(bas.mat.list, GenInd)
+colnames(sub.indices) <- c("Top25", "Top50", "Top75", "ALL")
+sub.indices
+
+sub.troph <- lapply(bas.mat.list, TrophInd)
+subs <- c("Top25", "Top50", "Top75", "ALL")
+for(i in 1:4){
+  sub.troph[[i]] <- cbind(sub.troph[[i]], web = factor(subs[i]))
+}
+
+sub.troph <- do.call(rbind, sub.troph)
+
+# Note that as you increase the number of weak interactions that are included, the more
+# species appear to be in higher trophic levels
+require(ggplot2)
+ggplot(sub.troph, aes(x = TL, y = ..density..)) + geom_histogram() + facet_wrap(~web, nrow = 2, ncol = 2)
